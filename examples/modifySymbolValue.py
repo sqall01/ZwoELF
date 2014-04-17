8ff5f9a5421ee5eae1419d9652039f502a7d0379
Andre Pawlowski <sqall@towel.hq.h4des.org>
2014-04-17 22:57:55 +0200

    added one line to header (because lib is used as third party lib in another project)

--- examples/modifySymbolValue.py
+++ examples/modifySymbolValue.py
@@ -3,6 +3,7 @@
 # written by sqall
 # twitter: https://twitter.com/sqall01
 # blog: http://blog.h4des.org
+# github: https://github.com/sqall01
 # 
 # Licensed under the GNU Public License, version 2.
 
