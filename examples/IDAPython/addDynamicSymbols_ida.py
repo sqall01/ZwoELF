067aacef9ee6ae8bab11c38211a437740bfc4a13
Andre Pawlowski <sqall@towel.hq.h4des.org>
2014-04-21 00:45:10 +0200

    added example to restore external calls like "malloc()" when sections are deleted from the ELF file

---
+++ examples/IDAPython/addDynamicSymbols_ida.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# written by sqall
+# twitter: https://twitter.com/sqall01
+# blog: http://blog.h4des.org
+# github: https://github.com/sqall01
+# 
+# Licensed under the GNU Public License, version 2.
+
+from ZwoELF import ElfParser
+from idautils import *
+
+currentFile = GetInputFilePath()
+elfFile = ElfParser(currentFile)
+
+# rename all symbols from the jump entries in ida
+for jmpRelEntry in elfFile.jumpRelocationEntries:
+	name = jmpRelEntry.symbol.symbolName
+
+	print "Add references for symbol: %s (0x%x)" % (name, jmpRelEntry.r_offset)
+	MakeRptCmt(jmpRelEntry.r_offset, "%s (restored by script)" % name)
+	dataRefs = DataRefsTo(jmpRelEntry.r_offset)
+
+	# get address of the data reference (usually there is only one reference)
+	address = list(dataRefs)[0]
+
+	# rename address
+	MakeName(address, name + "__restored")
