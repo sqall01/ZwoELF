8b9ec98305558ed8194aebb995f3bb7cc07c1683
johnLate <inbox-121@johnlate.scnr.net>
2014-09-26 17:13:45 +0200

    make struct.unpack(fmt, my_bytearray) work with older python

--- ZwoELF/__init__.py
+++ ZwoELF/__init__.py
@@ -7,6 +7,7 @@
 #
 # Licensed under the GNU Public License, version 2.
 
+import Compatibility
 from ElfParserLib import ElfParser, Section, Segment
 from Elf import ElfN_Ehdr, Shstrndx, Elf32_Shdr, SH_flags, SH_type, \
 	Elf32_Phdr, P_type, P_flags, D_tag, ElfN_Dyn, ElfN_Rel, ElfN_Sym, R_type, \
